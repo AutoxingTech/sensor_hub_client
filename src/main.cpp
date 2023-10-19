8551ab3d8bb1fd202fbf36e61b2c81c7445df9b0
liudf <liudf@autoxing.com>
2023-10-19 14:57:43 +0800

    add  debug log

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "sensor_hub_client");
 
+    const std::string uri = ros::master::getURI();
+    printf("master uri is %s\n", uri.c_str());
+    ROS_INFO("master url is %s", uri.c_str());
+
     // 创建TPC client 需要做自动重连
 
     SensorHubClient client;
